2e8df10fe748eecaf1d7bba71133a01db8202fbc
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-04-02 11:08:36 +0300

    add controller and model for list of videos in video-service

---
+++ video-service/src/main/java/com/codecool/videoservice/controllers/VideoController.java
@@ -0,0 +1,21 @@
+package com.codecool.videoservice.controllers;
+
+import com.codecool.videoservice.models.VideoCollection;
+import com.codecool.videoservice.models.VideoItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/")
+public class VideoController {
+
+    @RequestMapping("/list")
+    public VideoCollection getVideoList() {
+        return new VideoCollection(Arrays.asList(new VideoItem(1, "bla", "bla bla"),
+                new VideoItem(2, "blaa", "blaa blaa")));
+    }
+
+}
---
+++ video-service/src/main/java/com/codecool/videoservice/models/VideoCollection.java
@@ -0,0 +1,16 @@
+package com.codecool.videoservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class VideoCollection {
+
+    List<VideoItem> videoList;
+
+}
